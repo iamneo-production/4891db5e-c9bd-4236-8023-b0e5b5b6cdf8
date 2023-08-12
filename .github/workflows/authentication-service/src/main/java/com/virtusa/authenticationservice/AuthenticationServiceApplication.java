b2ecd7e815447bb9408cba62c066cdce6df5c626
Atuljay515 <atuljaybhaye3di@gmail.com>
2023-08-12 05:21:23 +0000

    intial commit

--- .github/workflows/authentication-service/src/main/java/com/virtusa/authenticationservice/AuthenticationServiceApplication.java
+++ .github/workflows/authentication-service/src/main/java/com/virtusa/authenticationservice/AuthenticationServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationServiceApplication.class, args);
+		SpringApplication.run(AuthenticationServiceApplication.class, args);		
 	}
 
 }
