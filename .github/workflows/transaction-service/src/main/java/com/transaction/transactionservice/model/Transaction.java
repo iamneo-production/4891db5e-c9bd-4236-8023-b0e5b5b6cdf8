a56e3f2cd9368d8a8600ab40964c64e615296c75
VethaShana <vvvttt479@gmail.com>
2023-08-12 06:58:42 +0000

    Add model transaction

---
+++ .github/workflows/transaction-service/src/main/java/com/transaction/transactionservice/model/Transaction.java
@@ -0,0 +1,39 @@
+package com.transaction.transactionservice.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="transaction")
+public class Transaction{
+
+    //Trans ID, Source Acc no, Destination Acc no, transfer amount , Date , time, Trans status
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer transactionId;
+
+    private Long sourceAccountNumber;
+
+    private Long destinationAccountNumber;
+
+    private Double transferAmount;
+
+    private LocalDateTime transactinDateTime; 
+
+    private Boolean transactionStatus;
+
+}
