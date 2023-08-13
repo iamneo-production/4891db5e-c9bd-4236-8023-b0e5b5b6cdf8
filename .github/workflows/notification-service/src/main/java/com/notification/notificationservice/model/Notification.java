31d62fc0fe13b898df30be62390e0bcbbdd1db9e
ruwaniprasadini <prasadiniruwani8@gmail.com>
2023-08-13 16:50:26 +0000

    ad changes

---
+++ .github/workflows/notification-service/src/main/java/com/notification/notificationservice/model/Notification.java
@@ -0,0 +1,30 @@
+package com.notification.notificationservice.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String senderEmail;
+    private Long senderAccNo;
+  
+    private LocalDateTime sentTimestamp;
+    
+   
+}
---
+++ .github/workflows/notification-service/src/main/java/com/notification/notificationservice/service/NotificationService.java
@@ -0,0 +1,93 @@
+package com.notification.notificationservice.service;
+
+import java.time.LocalDateTime;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import com.notification.notificationservice.Repository.NotificationRepository;
+import com.notification.notificationservice.model.Notification;
+
+
+
+@Service
+public class NotificationService {
+
+
+    @Autowired
+   
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    
+
+public void sendNotification(String senderEmail, Long senderAccNo) {
+        Notification notification = new Notification();
+        notification.setSenderEmail(senderEmail);
+        notification.setSenderAccNo(senderAccNo);
+       
+   
+
+ 
+
+        notificationRepository.save(notification);
+
+ 
+
+        sendEmailNotification(notification);
+    }
+
+
+
+private void sendEmailNotification(Notification notification) {
+
+    MimeMessage message = javaMailSender.createMimeMessage();
+    MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+
+
+    String accountNumber = 
+          String.valueOf(notification.getSenderAccNo());
+          
+
+ 
+
+            javaMailSender.send(message);
+            notification.setSentTimestamp(LocalDateTime.now());
+            notificationRepository.save(notification);
+         
+   
+}
+
+ 
+
+    /*public void sendEmailNotification(Notification notification) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+
+ 
+
+        
+            helper.setTo(notification.getSenderName());
+            helper.setSubject(notification.getSenderAccNo());
+           
+
+ 
+
+            javaMailSender.send(mimeMessage);
+            notification.setSentTimestamp(LocalDateTime.now());
+            notificationRepository.save(notification);
+         
+            
+
+        
+    }
+    */
+    
+}
